14c668efa53e79705fa76a52297d5a6c578012a2
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2022-01-12 23:09:15 +0300

    adjust events to button

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 var header = document.querySelector('header');
 
+var menu = document.querySelector('.menu');
+var open_btn = document.querySelector('.menu-btn');
+var close_btn = document.querySelector('.close-btn');
+
 window.addEventListener('scroll', displayheader);
+open_btn.addEventListener('click', addactive);
+close_btn.addEventListener('click', removeactive);
 
 function displayheader(){
     header.classList.toggle('sticky', window.scrollY > 0);
+}
+
+function addactive(){
+    menu.classList.add('active');
+}
+
+function removeactive(){
+    menu.classList.remove('active');
 }
